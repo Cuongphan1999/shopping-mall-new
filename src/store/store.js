4e1462118957bedf09510f86b87c4b278dc34014
phan cuong <phanxuancuong00677@gmail.com>
2024-02-18 19:50:56 +0700

    update

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsSlice from "../redux-toolkit/productsSlice";
-import filterSlice from "../redux-toolkit/filterSlice";
 import filtersSlice from "../redux-toolkit/filterSlice";
 //store co configurationstore
 const store = configureStore({
-    reducer: {
+    reducer: { //tap hop slice
         products: productsSlice.reducer, //reducer tap hop cac slide
         filters: filtersSlice.reducer
     }
