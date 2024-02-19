da3f9f41823164e45f57a49ae88ef6e3d895c1c8
phan cuong <phanxuancuong00677@gmail.com>
2024-02-19 21:22:37 +0700

    done shopping build on redux toolkit

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -5,12 +5,26 @@ import { FaArrowLeft } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { cartSelector } from "../store/selectors";
 import { formatCurrency } from "../helper/helper";
-import cartSlice from "../redux-toolkit/cartSlice";
+import cartSlice, { checkoutThunkAction } from "../redux-toolkit/cartSlice";
 import { toast } from "react-toastify";
+import { useForm } from 'react-hook-form'
+import Swal from "sweetalert2";
+import * as yup from 'yup'
+import { yupResolver } from "@hookform/resolvers/yup"
+
+const schema = yup.object({
+    fullname: yup.string().required(),
+    mobile: yup.string().required(),
+    email: yup.string().email().required(),
+    address: yup.string().required()
+})
 
 function CartPage() {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm({
+        resolver: yupResolver(schema)
+    })
     const dispatch = useDispatch()
-    const {cartInfor, cartDetails} = useSelector(cartSelector)
+    const {cartId, cartCustomerInfor, cartInfor, cartDetails} = useSelector(cartSelector)
     const handleIncrementQuantity = (cartItem) => {
         if (cartItem?.quantity < cartItem?.stock) {
             dispatch(cartSlice.actions.incrementQuantity(cartItem.id))
@@ -18,6 +32,65 @@ function CartPage() {
         else {
             toast.warning(`You can not by this product over ${cartItem?.stock}`)
         }
+    }
+    const handleDescrementQuantity = (cartItem) => {
+        if (cartItem?.quantity > 1) {
+            dispatch(cartSlice.actions.decrementQuantity(cartItem.id))
+        }
+        else {
+            toast.warning(`You cannot descrement quantity less than 1`)
+        }
+    }
+    const handleRemoveCartItem = (cartItem) => {
+        Swal.fire({
+            title: `Are you sure to remove${cartItem?.title}?`,
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, remove it!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              
+                dispatch(cartSlice.actions.removeCartItem(cartItem?.id))
+                toast.info(`${cartItem?.title} removed form cart success`)
+              
+            }
+          });
+    }
+    const handleCreateCustomerInfo = async (values) => {
+        //dispatch(cartSlice.actions.createCustomerInfor(values))
+        Swal.fire({
+            title: `Are you sure to Checkout?`,
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Confirm"
+          }).then((result) => {
+            if (result.isConfirmed) {
+                //dispatch(cartSlice.actions.createCustomerInfor(values))
+               const data = {
+                cartId: cartId,
+                cartInfor: {
+                    ...cartInfor
+                },
+                cartDetails: [
+                    ...cartDetails
+                ],
+                customerInfor: {
+                    ...values
+                }
+
+               }
+               dispatch(checkoutThunkAction(data))
+                toast.info(`Cart checkout success`)
+              reset()
+            }
+          });
+    
     }
     return (
         <MainLayout>
@@ -62,7 +135,7 @@ function CartPage() {
                                                     <div className="cart-quantity">
                                                         <span
                                                             role="button"
-                                                            //onClick={() => handleDescrementQuantity(cartItem)}
+                                                            onClick={() => handleDescrementQuantity(cartItem)}
                                                         >-</span>
                                                         <span>{cartItem?.quantity}</span>
                                                         <span
@@ -79,7 +152,7 @@ function CartPage() {
                                             <td>
                                                 <div className="action-wrap">
                                                     <span className="btn-remove"
-                                                        //onClick={() => handleRemoveCartItem(cartItem)}
+                                                        onClick={() => handleRemoveCartItem(cartItem)}
                                                     >&times;</span>
                                                 </div>
                                             </td>
@@ -116,9 +189,52 @@ function CartPage() {
                                 <span className="fw-bolder fs-6">{formatCurrency(cartInfor?.total)}</span>
                             </div>
                         </div>
-                        <div className="py-3 bg-success mt-2 d-flex align-items-center justify-content-center text-white btn-checkout">
-                            CHECKOUT
-                        </div>
+                        <form onSubmit={handleSubmit(handleCreateCustomerInfo)}>
+                            <div className="customer-info p-3">
+                                <h3 className="border-bottom py-2">Customer Info</h3>
+                                <div className="form-group mb-3">
+                                    <label className="form-label">Fullname</label>
+                                    <input type="text"
+                                        className={`${errors.fullname?.message ? 'is-invalid' : ''} form-control`}
+                                        placeholder="Fullname"
+                                        {...register('fullname')}
+                                    />
+                                     <span className="invalid-feedback">{errors.fullname?.message}</span>
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label className="form-label">Address</label>
+                                    <input type="text"
+                                        className={`${errors.address?.message ? 'is-invalid' : ''} form-control`}
+                                        placeholder="Address"
+                                        {...register('address')}
+                                    />
+                                    <span className="invalid-feedback">{errors.address?.message}</span>
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label className="form-label">Email</label>
+                                    <input type="text"
+                                        className={`${errors.email?.message ? 'is-invalid' : ''} form-control`}
+                                        placeholder="Email"
+                                        {...register('email')}
+                                    />
+                                    <span className="invalid-feedback">{errors.email?.message}</span>
+                                </div>
+                                <div className="form-group mb-3">
+                                    <label className="form-label">Mobile</label>
+                                    <input type="text"
+                                        className={`${errors.mobile?.message ? 'is-invalid' : ''} form-control`}
+                                        placeholder="Mobile"
+                                        {...register('mobile')}
+                                    />
+                                    <span className="invalid-feedback">{errors.mobile?.message}</span>
+                                </div>
+                            </div>
+                            <div className="py-3 bg-success mt-2 d-flex align-items-center justify-content-center text-white btn-checkout">
+                                <button type="submit" className="btn btn-block flex-grow-1">
+                                    CHECKOUT
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- src/redux-toolkit/cartSlice.js
+++ src/redux-toolkit/cartSlice.js
@@ -13,7 +13,7 @@
     + thu dien tu(email)
     + sdt(mobile)
 */
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { v4 as uuid } from 'uuid'
 
 
@@ -35,7 +35,7 @@ const cartSlice = createSlice({
             mobile: ''
         }
     },
-    reducers:{
+    reducers: {
         addToCart: (state, action) => {
             /*
             th1: sp do chua co trong gio
@@ -48,9 +48,9 @@ const cartSlice = createSlice({
             tinh lai total
              */
             let cartItem = state.cartDetails.find((item) => item.id === action.payload.id)
-           //console.log(cartItem)
+            //console.log(cartItem)
             //sp da co trong gio hang
-            if(cartItem?.id){
+            if (cartItem?.id) {
                 cartItem.quantity = Number(cartItem.quantity) + 1
                 cartItem.amount = Number(cartItem.quantity) * Number(cartItem.newPrice)
             }
@@ -62,7 +62,7 @@ const cartSlice = createSlice({
                 })
             }
             let newSubtotal = 0
-            for(let item of state.cartDetails){
+            for (let item of state.cartDetails) {
                 newSubtotal += Number(item.amount)
             }
             let newTotal = newSubtotal + Number(state.cartInfor.shipping)
@@ -71,7 +71,7 @@ const cartSlice = createSlice({
         },
         incrementQuantity: (state, action) => {
             let cartItem = state.cartDetails.find((item) => item.id === action.payload)
-            cartItem.quantity = Number(cartItem.quantity) + 1
+            cartItem.quantity = Number(cartItem.quantity) + 1 //test tang 10 lan hien error
             cartItem.amount = Number(cartItem.quantity) * Number(cartItem.newPrice)
 
             let newSubtotal = 0
@@ -81,7 +81,71 @@ const cartSlice = createSlice({
             let newTotal = newSubtotal + Number(state.cartInfor.shipping)
             state.cartInfor.subtotal = newSubtotal;
             state.cartInfor.total = newTotal
-        }
-    }
+        },
+        decrementQuantity: (state, action) => {
+            let cartItem = state.cartDetails.find((item) => item.id === action.payload)
+            cartItem.quantity = Number(cartItem.quantity) - 1 //test tang 10 lan hien error
+            cartItem.amount = Number(cartItem.quantity) * Number(cartItem.newPrice)
+
+            let newSubtotal = 0
+            for (let item of state.cartDetails) {
+                newSubtotal += Number(item.amount)
+            }
+            let newTotal = newSubtotal + Number(state.cartInfor.shipping)
+            state.cartInfor.subtotal = newSubtotal;
+            state.cartInfor.total = newTotal
+        },
+        removeCartItem: (state, action) => {
+            state.cartDetails = state.cartDetails.filter((item) => item.id !== action.payload)
+            let newSubtotal = 0
+            for (let item of state.cartDetails) {
+                newSubtotal += Number(item.amount)
+            }
+            let newTotal = newSubtotal + Number(state.cartInfor.shipping)
+            state.cartInfor.subtotal = newSubtotal;
+            state.cartInfor.total = newTotal
+        },
+        // createCustomerInfor: (state, action) => {
+        //     state.customerInfor = action.payload
+        // }
+
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(checkoutThunkAction.pending, (state, action) => {
+
+        })
+        .addCase(checkoutThunkAction.fulfilled, (state, action) => {
+            state.cartId = uuid();
+            state.cartInfo = {
+                subtotal: 0,
+                shipping: 0,
+                total: 0,
+                orderDate: new Date().valueOf()
+            };
+            state.cartDetails = []
+            state.customerInfo = {
+                fullname: '',
+                address: '',
+                email: '',
+                mobile: ''
+            }
+        })
+}
 })
+export const checkoutThunkAction = createAsyncThunk('cart/checkout', async (data) => {
+    let checkoutRes = await fetch('https://student-app-api.vercel.app/orderList', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    let result = await checkoutRes.json()
+    console.log(result)
+    return result
+})
+
 export default cartSlice
+
+//1.31.57
